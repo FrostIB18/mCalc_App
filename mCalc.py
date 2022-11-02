61743a355ad19db861b7f4b20c22c5c1ba8f2126
Ivan Burakov <ivan.burakov.1998@mail.ru>
2022-11-02 13:01:04 +0600

    fixing fonts

--- mCalc.py
+++ mCalc.py
@@ -7,16 +7,18 @@ class mCalcApp(App):
 
     def build(self):
         self.formula = "0"
-        main_layout = BoxLayout(orientation='vertical', padding=20, spacing=5)
+        main_layout = BoxLayout(orientation='vertical', padding=20, spacing=15)
         self.solution = TextInput(multiline=False, background_color='black', foreground_color='white',
-                                  readonly=False, halign='left', font_size=50, text='0')
+                                  readonly=False, halign='left', font_size=100, text='0')
         main_layout.add_widget(self.solution)
 
         self.fast_solution = TextInput(multiline=False, background_color='black', foreground_color='white',
-                                       readonly=True, halign='right', font_size=50)
+                                       readonly=True, halign='right', font_size=100)
         if self.solution.text != "0":
             self.fast_solution.text = self.solution.text
+        main_layout.add_widget(BoxLayout())
         main_layout.add_widget(self.fast_solution)
+
 
         buttons = [
             ["C", "(", ")", "*"],
@@ -27,16 +29,16 @@ class mCalcApp(App):
         ]
 
         for row in buttons:
-            h_layout = BoxLayout(spacing=5)
+            h_layout = BoxLayout(spacing=10)
             for label in row:
                 if label == "C":
-                    button = Button(text=label, color='red', font_size=30, pos_hint={"center_x": 0.5, "center_y": 0.5},
+                    button = Button(text=label, color='red', font_size=100, pos_hint={"center_x": 0.5, "center_y": 0.5},
                                     on_press=self.on_button_press)
                 elif label == "<<":
-                    button = Button(text=label, color='yellow', font_size=30, pos_hint={"center_x": 0.5, "center_y": 0.5},
+                    button = Button(text=label, color='yellow', font_size=100, pos_hint={"center_x": 0.5, "center_y": 0.5},
                                     on_press=self.on_button_press)
                 else:
-                    button = Button(text=label, font_size=30, pos_hint={"center_x": 0.5, "center_y": 0.5},
+                    button = Button(text=label, font_size=100, pos_hint={"center_x": 0.5, "center_y": 0.5},
                                     on_press=self.on_button_press)
                 h_layout.add_widget(button)
             main_layout.add_widget(h_layout)
